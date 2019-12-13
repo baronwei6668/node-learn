33406958bcff5258063b657812cea68ab50ae249
baronwei <baronwei@outlook.com>
2019-12-13 22:46:20 +0800

    u

--- nyc.config.js
+++ nyc.config.js
@@ -1,16 +1,15 @@
 'use strict'
-const { defaultExclude } = require('test-exclude')
-const isWindows = require('is-windows')
-
-let platformExclude = [isWindows() ? 'lib/posix.js' : 'lib/win32.js']
+// const { defaultExclude } = require('test-exclude')
+// const isWindows = require('is-windows')
+// let platformExclude = [isWindows() ? 'lib/posix.js' : 'lib/win32.js']
 
 module.exports = {
   all: false,
   'check-coverage': true,
   reporter: ['lcov', 'text-summary'],
   'report-dir': './.nyc_output/coverage',
-  'temp-dir': './.nyc_output/temp',
+  'temp-dir': './.nyc_output/temp'
   // include:
   // skip-full
-  exclude: platformExclude.concat(defaultExclude)
+  // exclude: platformExclude.concat(defaultExclude)
 }
